3122509fb33ce6ad7b2c75d1090d40019dac5fb9
Muhammad Adam Firmansyah <adamf09002001@gmail.com>
2025-06-24 19:49:14 +0700

    first commit

---
+++ src/main/java/com/juaracoding/selenium/BrowserManager.java
@@ -0,0 +1,14 @@
+package com.juaracoding.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BrowserManager {
+    public static WebDriver build() {           // boleh build atau make
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+
+        return new ChromeDriver(options);
+    }
+}
